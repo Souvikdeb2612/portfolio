d0147451dbd623706c2e35ffcd7b03a7abb40c89
Souvikdeb2612 <souvikdeb26@gmail.com>
2021-01-06 21:52:26 +0530

    try php fix

--- contact.php
+++ contact.php
@@ -1,19 +1,22 @@
-<?php  
+<?php
+    $to ="souvikdeb26@gmail.com"
+    $subject ='New potfolio message';  
+
     $name=$_POST['name'];
-    $visitor_email=$_POST['email'];
+    $email=$_POST['email'];
     $message =$_POST['message'];
 
     $mail_from='deb.souvik2612@gmail.com';
-    $email_subject ='New potfolio message';
-    $email_body="Username: $name.\n".
-                "User Email: $visitor_email.\n".
-                "User Message: $message.\n";
+    
+    // $email_body="Username: $name.\n".
+    //             "User Email: $email.\n".
+    //             "User Message: $message.\n";
 
-    $to ="souvikdeb26@gmail.com"
-    $headers="From: $email_from\r\n";
-    $headers ="Reply-to:$visitor_email\r\n";
+    
+    $headers .="From: $email\r\n";
 
-    mail($to,$email_subject,$email_body,$headers);
+    mail($to,$subject,$message,$headers);
+    echo"Email has been sent! Thank you $name";
     header("Location:index.html");
 
 
